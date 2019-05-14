82f22546be6786c01e976619c5be40396287892c
houzhenpu <houzhenpu@jiemian.com>
2019-05-14 16:55:12 +0800

    【flutter_fullhtml_textview】删除不必要的import

--- lib/htmlText/html_parser.dart
+++ lib/htmlText/html_parser.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/htmlText/constants.dart';
 import 'package:flutter_app/htmlText/flutter_html_text.dart';
